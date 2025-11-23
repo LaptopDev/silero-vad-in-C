34fa1f0074179bcfc72de2c20978abc7c3b16468
LaptopDev <dgibbons165@gmail.com>
2025-11-22 22:01:15 -0600

    Add keybinding to play last transcribed audio

---
+++ desktop_player/config.h
@@ -0,0 +1,2 @@
+    // play last transcribed audio with mpv and exit the script if the process ends by monitoring dbus.
+    { MODKEY|ControlMask, XK_p, spawn, SHCMD("/home/user/system-scripts/play_last_recording.sh") },
